2fca99535ee69c658917666bc0be099f58651383
Amol Budhiraja <abudhiraja@berkeley.edu>
2024-10-20 07:14:16 -0700

    Fixed UI Sizes

--- Tree Generation/Assets/CrashingOut.js
+++ Tree Generation/Assets/CrashingOut.js
@@ -35,8 +35,8 @@ collider.onOverlapEnter.add(function (e) {
    
     // Format pull request confirmation window
     script.confirmWindow.enabled = true;
-    script.confirmOption1.text = "Merge " + branch_small + "/" + hash_small.slice(0, 4) + " into " + branch_large + "/" + hash_large.slice(0, 4);
-    script.confirmOption2.text = "Merge " + branch_large + "/" + hash_large.slice(0, 4) + " into " + branch_small + "/" + hash_small.slice(0, 4);
+    script.confirmOption1.text = "Merge " + branch_large + "/" + hash_large.slice(0, 4) + " into " + branch_small + "/" + hash_small.slice(0, 4);
+    script.confirmOption2.text = "Merge " + branch_small + "/" + hash_small.slice(0, 4) + " into " + branch_large + "/" + hash_large.slice(0, 4);
    
     // global.behaviorSystem.sendCustomTrigger('pull_request');
  
