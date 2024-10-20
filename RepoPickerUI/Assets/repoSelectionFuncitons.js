92769227bf33843e262b17126036c98daefe4e09
Gregory Shklovski <gshklovs@uci.edu>
2024-10-20 02:47:43 -0700

    added UI for picking a repo as RepoPickerUI

--- RepoPickerUI/Assets/repoSelectionFuncitons.js
+++ RepoPickerUI/Assets/repoSelectionFuncitons.js
@@ -1,15 +1,18 @@
 
 script.onRepoSelection1 = function () {
+    // ro curgoatify
     print("Name: ")
     print("Repo 1 Selected! ");
 };
 
 script.onRepoSelection2 = function () {
+    // gshklovs Irvinehacks
     print("Name: ")
     print("Repo 2 Selected! ");
 };
 
 script.onRepoSelection3 = function () {
+    // 
     print("Name: ")
     print("Repo 3 Selected! ");
 };
